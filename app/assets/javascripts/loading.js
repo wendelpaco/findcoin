dedcab11722295a6665cb12c96b354f0413df68c
Wendel Paco <wendelpaco@live.com>
2021-04-19 10:14:59 -0300

    bug fix

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
       $(this).attr('disabled',true)
       location.reload(true)
       console.info('recarregando a tela para atualizar os dados')
-    },5000)
+    },10000)
   })
   //setInterval(()=> {$('#onLoading').trigger('click')},10000)
   $('.col-md-3').click(function(cellIndex){
     $(this).find(".card-custom").each(function() {
        $(this).find(".fix span").each(function() {
         var title = $(this).attr("title");
-        window.open(`https://coinmarketcap.com/currencies/${title}/`, "_blank")
+        window.open("https://coinmarketcap.com/currencies/"+ title, "_blank")
       });      
     });
   }) 
